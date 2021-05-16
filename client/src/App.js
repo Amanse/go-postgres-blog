40ac3a184273fdf63b419d03384801489f786a13
Aman <amansetiarjp@gmail.com>
2021-05-16 22:29:05 +0530

    Client with react pwa template

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import Posts from "./components/Posts"
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Posts />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
